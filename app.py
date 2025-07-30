21da98faee65eb2e8b81cce1081aab161c5c5614
Sainath Reddy <58382420+SainathReddy3155@users.noreply.github.com>
2025-07-29 23:12:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@
     app.config["MAIL_SERVER"]='smtp.gmail.com'
     app.config["MAIL_PORT"]=465
     app.config['MAIL_USERNAME'] = 'sainathreddy.unf12@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'mvjc tpri giio ltyf'
+    app.config['MAIL_PASSWORD'] = 'wew hdfd her ead'
     app.config['MAIL_USE_TLS'] = False
     app.config['MAIL_USE_SSL'] = True
     mail = Mail(app)
